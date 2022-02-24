c4d98be443e588a10a81dccee6efaf4f1ea43a64
Alan <71236097+AlanAngeli@users.noreply.github.com>
2022-02-23 22:58:09 -0300

    Add files via upload

---
+++ sets2.py
@@ -0,0 +1,12 @@
+"""
+
+add (adiciona), update (atualiza), clear, discard
+union | (une)
+intersection & (todos os elementos presentes nos dois sets)
+difference - (elementos apenas no set da esquerda)
+symmetric_difference ^ (Elementos que estão nos dois sets, mas não em ambos)
+
+***diferentes da listas e tuplas, os sets só suportam elementos únicos!
+
+"""
+
