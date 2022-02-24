ea37cc3ee2821bfb67875851aa200ace898a849f
Alan <71236097+AlanAngeli@users.noreply.github.com>
2022-02-23 23:23:27 -0300

    Update sets2.py

--- sets2.py
+++ sets2.py
@@ -10,3 +10,14 @@ symmetric_difference ^ (Elementos que estão nos dois sets, mas não em ambos)
 
 """
 
+l1 = [1,2,3,4,5,6,7,7,7,7,7,8,9,9,10, "Alan", "tô com sono kct", "Alan"]
+print(l1)
+print()
+
+l1 = set(l1) #os itens não se repetem
+print(l1)
+print()
+
+l1 = list(l1)
+print(l1[-1]) #acessar o útlimo item da lista
+print()
